0d7667c47f5b7190c05d0b281539377399a5cb9b
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-18 09:49:44 +0100

    styling: reposition the call to action button for mobile layout

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'multi-form-step',
+  base: '/multi-form-step',
   plugins: [react()],
 })
