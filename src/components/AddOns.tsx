d16c704cff68c2afe5cdfac7898570c52a93678e
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-20 16:34:13 +0100

    added click handler

--- src/components/AddOns.tsx
+++ src/components/AddOns.tsx
@@ -4,14 +4,19 @@ import data from '../data.json'
 function AddOns() {
     const navigate = useNavigate()
 
+    const handleAddonClick: React.MouseEventHandler = e => {
+        const elem = e.currentTarget
+        console.log(elem)
+    }
+
     return (
         <div className="page three">
             <h1>Pick add-ons</h1>
             <p>Add-ons help enhance your gaming experience.</p>
 
             <div className="addons-list">
-                {data.addons.map(addon => (
-                    <div className="addon">
+                {data.addons.map((addon, i) => (
+                    <div className="addon" onClick={handleAddonClick} key={i}>
                         <input type="checkbox" name='addon' id='addon' />
 
                         <div className="content">
