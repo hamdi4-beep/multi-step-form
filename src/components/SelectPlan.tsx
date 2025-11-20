f9bda33ac6df99feebc41f94b478f411b64ed06a
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-20 12:26:18 +0100

    added dynamic payment cycle for different prices

--- src/components/SelectPlan.tsx
+++ src/components/SelectPlan.tsx
@@ -1,7 +1,9 @@
 import { Link, useNavigate } from 'react-router'
 import data from '../data.json'
+import { useState } from 'react'
 
 function SelectPlan() {
+    const [paymentCycle, setPaymentCycle] = useState<'monthly' | 'yearly'>('monthly')
     const navigate = useNavigate()
 
     return (
@@ -10,23 +12,24 @@ function SelectPlan() {
             <p>You have the option of monthly or yearly billing.</p>
 
             <div className="plans-list">
-                {data.plans.map(plan => (
-                    <div className={`plan ${plan.name === 'Arcade' ? 'active-plan' : ''}`}>
+                {data.plans.map((plan, i) => (
+                    <div className={`plan ${plan.name === 'Arcade' ? 'active-plan' : ''}`} key={i}>
                         <div className="avatar-img">
                             <img src={`/multi-step-form/images/icon-${plan.name.toLocaleLowerCase()}.svg`} alt="" />
                         </div>
 
                         <div className="plan-info">
                             <p>{plan.name}</p>
-                            <span>${plan.price}/mo</span>
+                            <span>${(plan.price as {[x: string]: number})[paymentCycle]}/{paymentCycle === 'monthly' ? 'mo' : 'yr'}</span>
+                            {paymentCycle === 'yearly' && <p className='gift-desc'>2 months free</p>}
                         </div>
                     </div>
                 ))}
             </div>
 
-            <div className="toggle">
+            <div className="toggle" onClick={() => setPaymentCycle(prev => prev === 'yearly' ? 'monthly' : 'yearly')}>
                 <span className="active">Monthly</span>
-                <div className="toggle-bar"></div>
+                <div className={`toggle-bar ${paymentCycle === 'yearly' ? 'toggled' : ''}`}></div>
                 <span>Yearly</span>
             </div>
 
