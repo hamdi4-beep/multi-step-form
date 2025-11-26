cb12deae4ca81568bc610090d962878ada617950
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-26 12:30:23 +0100

    added total price calculation

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,15 +1,13 @@
 import { useLocation, useNavigate } from "react-router"
 
-const paymentCycle = {
-    'monthly': 'mo',
-    'yearly': 'yr'
-} as Record<string, string>
-
 function Summary() {
     const navigate = useNavigate()
     const {state} = useLocation()
 
-    console.log(state)
+    const paymentCycle = state.paymentCycle === 'monthly' ?
+        'mo' : 'yr'
+
+    console.log()
 
     return (
         <div className="page four">
@@ -19,24 +17,24 @@ function Summary() {
             <div className="wrapper">
                 <div className="top-banner">
                     <div className="plan-info">
-                        <p className="plan-title">{state.selectedPlan['name']} ({state.paymentCycle === 'monthly' ? 'Monthly' : 'Yearly'})</p>
+                        <p className="plan-title">{state.selectedPlan['name']} ({state.paymentCycle.replace(paymentCycle[0], paymentCycle[0].toUpperCase())})</p>
                         <button className="change-plan-btn">Change</button>
                     </div>
 
-                    <p className="plan-price">${state.selectedPlan.price[state.paymentCycle]}/{paymentCycle[state.paymentCycle]}</p>
+                    <p className="plan-price">${state.selectedPlan.price[state.paymentCycle]}/{paymentCycle}</p>
                 </div>
 
                 {state.addons.map((addon: any, index: number) => (
                     <div className="addon-info" key={index}>
                         <p>{addon.title}</p>
-                        <p className="addon-price">+${addon.price[state.paymentCycle]}/{paymentCycle[state.paymentCycle]}</p>
+                        <p className="addon-price">+${addon.price[state.paymentCycle]}/{paymentCycle}</p>
                     </div>
                 ))}
             </div>
 
             <div className="bottom-banner">
                 <p className="total-label">Total (per year)</p>
-                <p className="total-price">$120/yr</p>
+                <p className="total-price">${state.selectedPlan.price[state.paymentCycle] + state.addons.reduce((prev: any, curr: any) => curr.price[state.paymentCycle] + prev.price[state.paymentCycle])}/{paymentCycle}</p>
             </div>
 
             <div className="action-btns">
