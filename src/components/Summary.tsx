164e7a1fe7de285a17ecf71d461fd6bad9df590b
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-11-27 12:44:56 +0100

    refactor: cleaned up codebase

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -23,18 +23,23 @@ type State = {
 
 function Summary() {
     const navigate = useNavigate()
-    const {state, pathname}: {
+
+    const {
+        state,
+        pathname
+    }: {
         state: State
         pathname: string
     } = useLocation()
+    
     const [isConfirmed, setIsConfirmed] = useState(false)
 
-    const paymentCycle = state.paymentCycle === 'monthly' ? 'mo' : 'yr'
+    const cycleVariations = state.paymentCycle === 'monthly' ? 'mo' : 'yr'
     const selectedPlanPrice = state.selectedPlan.price[state.paymentCycle]
 
     const addonsTotalPrice = state.addons
-            .map((it: any) => it.price[state.paymentCycle])
-            .reduce((prev: any, curr: any) => prev + curr)
+            .map(addon => addon.price[state.paymentCycle])
+            .reduce((prev, curr) => prev + curr)
 
     const handleChangeClick = () =>
         navigate(pathname, {
@@ -55,24 +60,24 @@ function Summary() {
             <div className="wrapper">
                 <div className="top-banner">
                     <div className="plan-info">
-                        <p className="plan-title">{state.selectedPlan['name']} ({state.paymentCycle.replace(paymentCycle[0], paymentCycle[0].toUpperCase())})</p>
+                        <p className="plan-title">{state.selectedPlan['name']} ({state.paymentCycle.replace(state.paymentCycle[0], state.paymentCycle[0].toUpperCase())})</p>
                         <button className="change-plan-btn" onClick={handleChangeClick}>Change</button>
                     </div>
 
-                    <p className="plan-price">${selectedPlanPrice}/{paymentCycle}</p>
+                    <p className="plan-price">${selectedPlanPrice}/{cycleVariations}</p>
                 </div>
 
-                {state.addons.map((addon: any, index: number) => (
+                {state.addons.map((addon, index) => (
                     <div className="addon-info" key={index}>
                         <p>{addon.title}</p>
-                        <p className="addon-price">+${addon.price[state.paymentCycle]}/{paymentCycle}</p>
+                        <p className="addon-price">+${addon.price[state.paymentCycle]}/{cycleVariations}</p>
                     </div>
                 ))}
             </div>
 
             <div className="bottom-banner">
-                <p className="total-label">Total (per {paymentCycle === 'mo' ? 'month' : 'year'})</p>
-                <p className="total-price">${selectedPlanPrice + addonsTotalPrice}/{paymentCycle}</p>
+                <p className="total-label">Total (per {cycleVariations === 'mo' ? 'month' : 'year'})</p>
+                <p className="total-price">${selectedPlanPrice + addonsTotalPrice}/{cycleVariations}</p>
             </div>
 
             <div className="action-btns">
